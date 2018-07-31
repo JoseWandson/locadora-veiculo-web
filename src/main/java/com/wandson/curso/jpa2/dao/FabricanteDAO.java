054aa1f025d4ea7f32e6b72063ad6d3c342aa71f
Wandson Cavalcante <wandsonacop@hotmail.com>
2018-07-30 23:22:54 -0300

    Implementação da consulta e exclusão de fabricantes

--- src/main/java/com/wandson/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/wandson/curso/jpa2/dao/FabricanteDAO.java
@@ -1,21 +1,36 @@
 package com.wandson.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.wandson.curso.jpa2.modelo.Fabricante;
+import com.wandson.curso.jpa2.service.NegocioException;
+import com.wandson.curso.jpa2.util.jpa.Transactional;
 
 public class FabricanteDAO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager em;
 
 	public void salvar(Fabricante fabricante) {
 		em.persist(fabricante);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
+	}
+
+	@Transactional
+	public void excluir(Fabricante fabricante) throws NegocioException {
+		fabricante = em.find(Fabricante.class, fabricante.getCodigo());
+		em.remove(fabricante);
+		em.flush();
 	}
 
 }
